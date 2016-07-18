a73b3c969b45b251a1a624c11790bb1498c354c5
thanhtt <tran.tien.thanh@framgia.com>
2016-07-18 16:20:14 +0700

    API Create Event

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   include ApplicationHelper
 
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
