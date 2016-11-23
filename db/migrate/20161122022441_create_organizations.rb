5780ee07573ae3b64f2016d5ce28e606b4627528
hongsondhs <hongsondhs@gmail.com>
2016-11-23 08:02:12 +0700

    Init organization

---
+++ db/migrate/20161122022441_create_organizations.rb
@@ -0,0 +1,10 @@
+class CreateOrganizations < ActiveRecord::Migration
+  def change
+    create_table :organizations do |t|
+      t.string :name
+      t.integer :owner_id
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010150010) do
+ActiveRecord::Schema.define(version: 20161122022709) do
 
   create_table "attendees", force: :cascade do |t|
     t.string   "email",      limit: 255
@@ -119,6 +119,13 @@ ActiveRecord::Schema.define(version: 20161010150010) do
     t.datetime "updated_at",                    null: false
   end
 
+  create_table "organizations", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.integer  "owner_id",   limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
   create_table "permissions", force: :cascade do |t|
     t.string   "permission", limit: 255
     t.datetime "created_at",             null: false
@@ -167,6 +174,16 @@ ActiveRecord::Schema.define(version: 20161010150010) do
 
   add_index "user_calendars", ["user_id", "calendar_id"], name: "index_user_calendars_on_user_id_and_calendar_id", unique: true, using: :btree
 
+  create_table "user_organizations", force: :cascade do |t|
+    t.integer  "user_id",         limit: 4
+    t.integer  "organization_id", limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  add_index "user_organizations", ["organization_id"], name: "index_user_organizations_on_organization_id", using: :btree
+  add_index "user_organizations", ["user_id"], name: "index_user_organizations_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "name",                   limit: 255
     t.string   "email",                  limit: 255, default: "",    null: false
@@ -198,4 +215,6 @@ ActiveRecord::Schema.define(version: 20161010150010) do
 
   add_foreign_key "notification_events", "events"
   add_foreign_key "notification_events", "notifications"
+  add_foreign_key "user_organizations", "organizations"
+  add_foreign_key "user_organizations", "users"
 end
